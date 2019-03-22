e7d020b34a628582d9e741d922704da6493c50de
Loic Sharma <sharma.loic@gmail.com>
2019-03-21 19:42:52 -0700

    Add project files.

---
+++ dotnet-develop/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace dotnet_develop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
